acb74ce7f3268b5518552fe9dd030e3e3c3bbbb4
MicroOptimization <jackiezhen538@gmail.com>
2023-04-14 16:44:22 -0400

    click display done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let clickDiv = document.getElementById("clickLocation")
+let clickLabel = document.getElementById("clLabel")
+document.body.addEventListener('click', function(event) {
+    console.log(clickDiv.style.display)
+    if (clickDiv.style.display === "none") {
+        console.log("h")
+        clickDiv.style.display === "block"
+    }
+
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log("clicked at " , x , y)
+    console.log(typeof x);
+
+    clickDiv.style.top = (y - 10).toString() + "px";
+    clickDiv.style.left = (x - 10).toString() + "px";
+    clickLabel.innerHTML = "Click Location: (" + x.toString() + ", " + y.toString() + ")";
+});
